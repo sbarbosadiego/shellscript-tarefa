bec0d31e6c70b8d10230033081924117eb9ef86f
Diego Barbosa <diego.barbosa.pro@gmail.com>
2023-10-20 08:53:00 -0300

    0.0.2 - Pronto para uso

--- tarefa.sh
+++ tarefa.sh
@@ -1,36 +1,83 @@
 #!/bin/bash
 
-# Atualizar pacote de dependencias linux
-atualizarLinux() {
-   while [[ ! "$atulizarPacotes" =~ [snSN] ]]; do
+# Atualizar pacotes e dependencias
+atualizar_Dependencias() {
+   while [[ ! "$atulizarPacotes" =~ [sSyYnN] ]]; do
       echo -n "Atualizar pacotes do sistema? [S/N] "
       read atulizarPacotes
    done
 
-   if [[ "$atulizarPacotes" =~ [sS] ]]; then
+   if [[ "$atulizarPacotes" =~ [sSyY] ]]; then
       sudo apt update -y
       sudo apt upgrade -y
    fi
 }
 
-# Atualizar repositório git
-atualizarGit() {
-   while [[ ! "$atulizarRepositorios" =~ [snSN] ]]; do
+# Limpar pacotes e dependencias
+limpar_Pacotes() {
+   while [[ ! "$limpar" =~ [sSyYnN] ]]; do
+      echo -n "Limpar pacotes não utilizados do sistema? [S/N] "
+      read limpar
+   done
+
+   if [[ "$limpar" =~ [sSyY] ]]; then
+      sudo apt autoclean -y
+      sudo apt autoremove -y
+   fi
+}
+
+# Atualizar repositorio git
+atualizar_Git() {
+   while [[ ! "$atulizarRepositorios" =~ [sSyYnN] ]]; do
       echo -n "Atualizar repositórios do Git? [S/N] "
       read atulizarRepositorios
    done
 
-   if [[ "$atulizarRepositorios" =~ [sS] ]]; then
-      cd /home/"$USER"/Git
-      pwd
-   for subdiretorio in */; do
-      echo "Atualizando o repositório: $subdiretorio"
-      cd "$subdiretorio"
-      git pull
-      cd ..
-   done  
+   if [[ "$atulizarRepositorios" =~ [sSyY] ]]; then
+
+      if test -e "/home/"$USER"/Git"; then
+         cd "/home/"$USER"/Git"
+         pwd
+         for subdiretorio in */; do
+            echo "Atualizando o repositório: $subdiretorio"
+            cd "$subdiretorio"
+            git pull
+            cd ..
+         done
+      else
+         echo "Diretório Git não encontrado"  
+      fi
+
+   fi  
+   
+}
+
+atualizar_Git() { 
+   if test -e "/home/$USER/Git"; then
+      
+      while [[ ! "$atulizarRepositorios" =~ [sSyYnN] ]]; do
+         echo -n "Atualizar repositórios do Git? [S/N] "
+         read atulizarRepositorios
+      done
+
+      if [[ "$atulizarRepositorios" =~ [sSyY] ]]; then
+
+         cd "/home/$USER/Git"
+         pwd
+
+         for subdiretorio in */; do
+            echo "Atualizando o repositório: $subdiretorio"
+            cd "$subdiretorio"
+            git pull
+            cd ..
+         done 
+      fi
+
+   else
+      echo "Diretório Git não encontrado"  
    fi
 }
 
-atualizarLinux
-atualizarGit
+atualizar_Dependencias
+limpar_Pacotes
+atualizar_Git
